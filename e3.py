091ad854f6cf74c9dfa81136bead947ca401e70c
Lizhong-Liu <lizhongliu@uchicago.edu>
2017-11-07 19:17:36 -0600

    DONE

--- e3.py
+++ e3.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Read in each csv and append them into a list.
 df_election = []
@@ -16,35 +17,18 @@ for i in range(1924,2020,4):
 
 # Concat the list to a dataframe and adding the Republican Share column.
 df_elections = pd.concat(df_election)
-df_elections["Republican Share"] = df_elections["Republican"]/df_elections["Total Votes Cast"]
-
-# Slicing out the county/city of interest, as well as the columns for plotting.
-df_accomack = df_elections.loc["Accomack County", ["Year", "Republican Share"]]
-df_albemarle = df_elections.loc["Albemarle County", ["Year", "Republican Share"]]
-df_alexandria = df_elections.loc["Alexandria City", ["Year", "Republican Share"]]
-df_alleghany = df_elections.loc["Alleghany County", ["Year", "Republican Share"]]
 
 # Plot and save the figures as PDF.
-df_accomack.plot(x = "Year", y = "Republican Share")
-plt.ylabel("Accomack County")
-plt.xticks(range(1924,2020,4),rotation = 90)
-plt.title("Republican Share of Accomack county")
-plt.savefig("accomack_county.pdf")
-
-df_albemarle.plot(x = "Year", y = "Republican Share")
-plt.ylabel("Albemarle County")
-plt.xticks(range(1924,2020,4),rotation = 90)
-plt.title("Republican Share of Albemarle County")
-plt.savefig("albemarle_county.pdf")
-
-df_alexandria.plot(x = "Year", y = "Republican Share")
-plt.ylabel("Alexandria City")
-plt.xticks(range(1924,2020,4),rotation = 90)
-plt.title("Republican Share of Alexandria City")
-plt.savefig("alexandria_city.pdf")
-
-df_alleghany.plot(x = "Year", y = "Republican Share")
-plt.ylabel("Alleghany County")
-plt.xticks(range(1924,2020,4),rotation = 90)
-plt.title("Republican Share of Alleghany County")
-plt.savefig("alleghany_county.pdf")
+city = ["Accomack County", "Albemarle County", "Alexandria City", "Alleghany County"]
+for i in city:
+    df_city = df_elections.loc[df_elections.index.str.contains(i), : ].groupby("Year").sum()
+    df_city["Republican Share"] = df_city["Republican"] / df_city["Total Votes Cast"]
+    df_city["Year"] = df_city.index
+    df_city.plot(x = "Year", y = "Republican Share")
+    plt.ylabel("Republican Vote Share")
+    plt.yticks(np.arange(0.0, 1.0, 0.2))
+    plt.xticks(range(1924, 2020, 4),rotation = 90)
+    plt.grid(True)
+    plt.title("Republican Vote Share of " + i)
+    p_name = i.lower().replace(" ", "_") + ".pdf"
+    plt.savefig(p_name)
